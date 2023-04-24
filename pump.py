1d874090921ac7e7803455816a1d32f81280fec8
Marcel <mrizzo25@gmail.com>
2023-04-23 22:37:23 -0300

    added error handling in get_price

--- pump.py
+++ pump.py
@@ -3,11 +3,12 @@ import requests
 symbols = ['BTCUSDT', 'ETHUSDT', 'ARBUSDT']
 
 def get_prices():
-  
-    response = requests.get('https://api.binance.com/api/v3/ticker/price')
-    prices = [pair for pair in response.json() if pair['symbol'] in symbols]
-    return prices
-           
+    try:
+        response = requests.get('https://api.binance.com/api/v3/ticker/price')
+        prices = [pair for pair in response.json() if pair['symbol'] in symbols]
+        return prices
+    except requests.exceptions.RequestException as e:
+        print(e)       
  
 prices = get_prices()
 print(prices) 
