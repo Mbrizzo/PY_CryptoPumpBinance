8581246dc974b111df949ebdd7ff0ccf5e612e37
Marcel <mrizzo25@gmail.com>
2023-04-26 08:54:39 -0300

    calculando media exp

--- volatilidade.py
+++ volatilidade.py
@@ -3,7 +3,7 @@ import json
 import math
 
 
-response = requests.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=10')
+response = requests.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=10')
 data = json.loads(response.text)
 close_prices = [float(d[4]) for d in data]
 
@@ -11,4 +11,10 @@ mean = sum(close_prices) / len(close_prices)
 variance = sum([((x - mean) ** 2) for x in close_prices]) / len(close_prices)
 volatility = math.sqrt(variance)
 
-print(volatility)
+ema = sum(close_prices[-9:]) / 9
+
+trend = 'alta' if close_prices[-1] > ema else 'baixa'
+
+print(f"Volatilidade: {volatility:.2f}")
+print(f"Média Exponencial de 9 períodos: {ema:.2f}")
+print(f"Tendência: {trend}")
