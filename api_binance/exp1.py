185f551c2c0eb82fd173505a267370759bbe1749
Marcel <mrizzo25@gmail.com>
2023-05-11 23:34:42 -0300

    add func crossover

--- api_binance/exp1.py
+++ api_binance/exp1.py
@@ -4,5 +4,22 @@ def get_prices():
     response = requests.get('https://api.binance.com/api/v3/ticker/price')
     return response.json()
 
-prices = get_prices()
-print(prices)
+#prices = get_prices()
+
+def crossover(symbol, start_time):
+    url = 'https://api.binance.com/api/v3/klines'
+    interval = "1m"
+    params = { 'symbol' : symbol,
+                'interval' : interval,
+                'startTime': start_time
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        klines = response.json()
+        return klines
+    else:
+        print("Error:", response.status_code)
+        return None
+
+cross= crossover('BTCUSDT', 1620753780000)
+print(cross)
