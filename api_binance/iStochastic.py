f7d44c043ca6fe288e56b30ed36ef1f70d31d84e
Marcel <mrizzo25@gmail.com>
2023-05-18 19:20:23 -0300

    get-pairs

--- api_binance/iStochastic.py
+++ api_binance/iStochastic.py
@@ -6,3 +6,20 @@ import os
 api_key = os.getenv('API_KEY')
 api_secret = os.getenv('API_SECRET')
 base_url = 'https://api.binance.com'
+
+def get_pairs():
+    endpoint = '/api/v3/exchangeInfo'
+    url = base_url + endpoint
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    trading_pairs = []
+    for symbol in response.json()['symbols']:
+        if symbol['quoteAsset'] == 'USDT':
+            trading_pairs.append(symbol['symbol'])
+
+    return trading_pairs
+
+get_pairs = get_pairs()
+print(get_pairs)
